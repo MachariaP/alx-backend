04a6b443078353dcd0ddb8e1b31fa175aea6f958
MachariaP <walburphinehas78@gmail.com>
2024-07-19 11:57:54 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module takes two integer arguments: page and page_size.
+Calculates and returns a tuple containing the start
+and end index for a slice of items.
+Page numbers are 1-indexed(first page is page 1.
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Calculates the start and end index for pagination.
+    
+    Parameters:
+        page (int): The current page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end index.
+    """
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
