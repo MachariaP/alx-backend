1eb80bf84546bdf00c73294aa0a2d273a7572115
MachariaP <walburphinehas78@gmail.com>
2024-07-24 23:57:32 +0300

    Pycodestyle Fix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -2,46 +2,55 @@
 """ LFU Caching module
 """
 from base_caching import BaseCaching
+from time import time
 
 
 class LFUCache(BaseCaching):
-    """ LFUCache inherits from BaseCaching and implements a caching system
-        using LFU and LRU algorithms.
+    """ LFUCache class that inherits from BaseCaching
+        and implements LFU caching
     """
+
     def __init__(self):
-        """ Initialize LFUCache.
+        """ Initialize LFUCache
         """
         super().__init__()
         self.usage_counts = {}  # Frequency of access
-        self.lru_tracker = {}  # Order of access
-        self.operation_counter = 0  # Order of operations
+        self.usage_order = {}  # Timestamp of last access
 
     def put(self, key, item):
-        """ Add or update a key-value pair. Discard the least
-            frequently used item if necessary.
+        """ Add an item in the cache
         """
         if key is None or item is None:
             return
+
         self.cache_data[key] = item
         self.usage_counts[key] = self.usage_counts.get(key, 0) + 1
-        self.operation_counter += 1
-        self.lru_tracker[key] = self.operation_counter
+        self.usage_order[key] = self._current_time()
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            least_used = min(self.usage_counts.items(),
-                             key=lambda x: (x[1], self.lru_tracker[x[0]]))
-            del self.cache_data[least_used[0]]
-            del self.usage_counts[least_used[0]]
-            del self.lru_tracker[least_used[0]]
-            print("DISCARD:", least_used[0])
+            # Find the least frequently used keys
+            min_freq = min(self.usage_counts.values())
+            lfu_keys = [k for k,
+                        v in self.usage_counts.items() if v == min_freq]
+
+            # Find the least recently used key among them
+            lru_key = min(lfu_keys, key=lambda k: self.usage_order[k])
+            del self.cache_data[lru_key]
+            del self.usage_counts[lru_key]
+            del self.usage_order[lru_key]
+            print("DISCARD:", lru_key)
 
     def get(self, key):
-        """ Return the value of the key if it exists, None otherwise.
-            Increment access count.
+        """ Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
+
         self.usage_counts[key] += 1
-        self.operation_counter += 1
-        self.lru_tracker[key] = self.operation_counter
+        self.usage_order[key] = self._current_time()
         return self.cache_data[key]
+
+    def _current_time(self):
+        """ Return the current time in milliseconds for ordering
+        """
+        return int(time() * 1000)
