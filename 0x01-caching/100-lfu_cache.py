00fe3cf761a41753ec346fa8ade4364369847993
MachariaP <walburphinehas78@gmail.com>
2024-07-24 18:04:06 +0300

    a class LFUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" LFU Caching module
+"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFUCache inherits from BaseCaching and implements a caching system
+        using LFU and LRU algorithms.
+    """
+    def __init__(self):
+        """ Initialize LFUCache.
+        """
+        super().__init__()
+        self.usage_counts = {}  # Frequency of access
+        self.lru_tracker = {}  # Order of access
+
+    def put(self, key, item):
+        """ Add or update a key-value pair. Discard the least
+            frequently used item if necessary.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        self.usage_counts[key] = self.usage_counts.get(key, 0) + 1
+        self.lru_tracker[key] = self.lru_tracker.get(key, 0) + 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            least_used = min(self.usage_counts.items(), key=lambda x: (x[1], self.lru_tracker[x[0]]))
+            del self.cache_data[least_used[0]]
+            del self.usage_counts[least_used[0]]
+            del self.lru_tracker[least_used[0]]
+            print("DISCARD:", least_used[0])
+
+    def get(self, key):
+        """ Return the value of the key if it exists, None otherwise.
+            Increment access count.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.usage_counts[key] += 1
+        self.lru_tracker[key] += 1
+        return self.cache_data[key]
