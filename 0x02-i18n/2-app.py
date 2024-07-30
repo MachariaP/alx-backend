7a8c265fb6a655f8b935a0d975851337a9c6106b
MachariaP <walburphinehas78@gmail.com>
2024-07-30 13:03:04 +0300

    Modified file: 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,7 +21,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Determine the best match for supported languages."""
-    return request.accept_languages.best_match(app.config.get('BABEL_SUPPORTED_LOCALES', ['en']))
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.context_processor
