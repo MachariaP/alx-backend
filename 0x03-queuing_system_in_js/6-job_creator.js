13f62b640c1e93ec01a95cf7cde2bb446ad98b06
MachariaP <walburphinehas78@gmail.com>
2024-08-28 11:29:20 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,25 @@
+import kue from 'kue';
+
+// Create a queue
+const queue = kue.createQueue();
+
+// Job data
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is a test message'
+};
+
+// Create a job in the 'push_notification_code' queue
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+// Event listeners for the job
+job.on('complete', () => {
+  console.log('Notification job completed');
+}).on('failed', () => {
+  console.log('Notification job failed');
+});
